6e084b7d38084351287fcebde776ae51f4c9797f
maxgotech <golovinmd@mail.ru>
2023-11-14 01:45:16 +0300

    експорт футера

--- src/app/shared/ui/footer-ui/footer-ui.module.ts
+++ src/app/shared/ui/footer-ui/footer-ui.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FooterUiComponent } from './footer-ui.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterUiComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[FooterUiComponent]
 })
 export class FooterUiModule { }
