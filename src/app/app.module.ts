1032f4e165975ce2cb86a6674730de32121e2a34
maxgotech <golovinmd@mail.ru>
2023-11-14 01:01:21 +0300

    к руту добавлены хэдер и футер

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderModule } from './shared/features/header/header.module';
+import { HomeComponent } from './home/feature/home.component';
+import { FooterUiComponent } from './shared/ui/footer-ui/footer-ui.component';
+import { FooterUiModule } from './shared/ui/footer-ui/footer-ui.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterUiComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HeaderModule,
+    FooterUiModule,
+    HttpClientModule,
+    RouterModule.forRoot([],{
+      scrollPositionRestoration:'enabled'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
