89a3d527e5637a52938eda5c1cb4bd8314c6e395
maxgotech <golovinmd@mail.ru>
2023-11-14 01:00:48 +0300

    Добавлена домашняя страница в роут

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: async () =>
+    (await import('./home/feature/home.module')).HomeModule
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
