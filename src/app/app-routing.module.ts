3a6aeff3d989cb37f6fd00ea0b7507c038cb8aff
maxgotech <golovinmd@mail.ru>
2023-11-16 17:49:23 +0300

    добавлен рут на каталог

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'catalog',
+    loadChildren: async () =>
+    (await import('./catalog/feature/catalog.module')).CatalogModule
+  },
+  {
     path: '',
     loadChildren: async () =>
     (await import('./home/feature/home.module')).HomeModule
