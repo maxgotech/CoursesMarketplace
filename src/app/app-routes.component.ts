f6e4bfa398bfd6cf2f983bf896583650674bdd2b
maxgotech <golovinmd@mail.ru>
2023-12-02 18:37:00 +0300

    добавлены роуты ЛК

--- src/app/app-routes.component.ts
+++ src/app/app-routes.component.ts
@@ -2,12 +2,16 @@ import { Routes } from "@angular/router";
 
 export const appRoutes: Routes = [
   {
+    path: 'user-profile',
+    loadChildren: async () =>(await import('./user-profile/feature/user-profile-routes/user-profile-routes.component'))
+  },
+  {
     path: 'catalog',
-    loadChildren: async () =>(await import('./catalog/feature/catalog-routing.component'))
+    loadChildren: async () =>(await import('./catalog/feature/catalog-routes.component'))
   },
   {
     path: '',
-    loadChildren: async () =>(await import('./home/feature/home-routing.component'))
+    loadChildren: async () =>(await import('./home/feature/home-routes.component'))
   },
   {
     path:'**',
