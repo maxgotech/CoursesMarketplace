77a9855f8715a546d8d83d4dd1d46b270f5319e0
maxgotech <golovinmd@mail.ru>
2023-12-02 23:13:40 +0300

    подключены компоненты на роуты страниц ЛК

--- src/app/user-profile/feature/user-profile-routes/user-profile-routes.component.ts
+++ src/app/user-profile/feature/user-profile-routes/user-profile-routes.component.ts
@@ -1,5 +1,7 @@
 import { UserProfileService } from "../../data-access/user-profile.service";
 import { UserProfileEditComponent } from "../user-profile-edit/user-profile-edit.component";
+import { UserProfileMailComponent } from "../user-profile-mail/user-profile-mail.component";
+import { UserProfilePassComponent } from "../user-profile-pass/user-profile-pass.component";
 
 export default [
   {
@@ -12,11 +14,11 @@ export default [
       },
       {
         path: 'mail',
-        component: UserProfileEditComponent
+        component: UserProfileMailComponent
       },
       {
         path: 'pass',
-        component: UserProfileEditComponent
+        component: UserProfilePassComponent
       }
     ]
   }
