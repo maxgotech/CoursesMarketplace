7943be0d38563f7c11c4360e3c9bf4cd711babab
maxgotech <golovinmd@mail.ru>
2023-12-02 23:03:19 +0300

    ui компонент страницы смены почты

---
+++ src/app/user-profile/ui/user-profile-mail-ui/user-profile-mail-ui.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-mail-ui',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './user-profile-mail-ui.component.html',
+  styleUrl: './user-profile-mail-ui.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserProfileMailUiComponent {
+  
+  @Input() mail:string = ''
+
+}
