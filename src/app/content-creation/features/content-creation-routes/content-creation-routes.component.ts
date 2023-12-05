60b63010149dc6dbcee31b6774dbd0558b06cc0a
maxgotech <golovinmd@mail.ru>
2023-12-05 21:13:43 +0300

    роут на контент занятия

--- src/app/content-creation/features/content-creation-routes/content-creation-routes.component.ts
+++ src/app/content-creation/features/content-creation-routes/content-creation-routes.component.ts
@@ -1,17 +1,32 @@
 import { CoursesService } from "../../data-access/courses/courses.service";
 import { StudiesService } from "../../data-access/studies/studies.service";
 import { CoursesCreationComponent } from "../courses/courses-creation/courses-creation.component";
+import { StudiesContentComponent } from "../studies/studies-content/studies-content.component";
 import { StudiesCreationComponent } from "../studies/studies-creation/studies-creation.component";
 
 export default [
   {
     path: 'courses',
     providers: [CoursesService],
-    component: CoursesCreationComponent
+    children:[
+      {
+        path:'',
+        component: CoursesCreationComponent,
+      }
+    ]
   },
   {
     path: 'studies',
     providers: [StudiesService],
-    component: StudiesCreationComponent
+    children:[
+      {
+        path:'',
+        component: StudiesCreationComponent
+      },
+      {
+        path:':id',
+        component:StudiesContentComponent
+      }
+    ]
   }
 ]
