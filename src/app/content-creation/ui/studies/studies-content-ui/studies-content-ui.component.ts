69aca663278bfbc7d4dd76e312b1d7f0403eb727
maxgotech <golovinmd@mail.ru>
2023-12-06 20:28:39 +0300

    обновлен ui компонент контента занятия

--- src/app/content-creation/ui/studies/studies-content-ui/studies-content-ui.component.ts
+++ src/app/content-creation/ui/studies/studies-content-ui/studies-content-ui.component.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { TextStudyUiComponent } from '../study-types/text-study-ui/text-study-ui.component';
 import { VideoStudyUiComponent } from '../study-types/video-study-ui/video-study-ui.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { StudyContentTextComponent } from 'src/app/content-creation/features/studies/study-content-types/study-content-text/study-content-text.component';
 
 @Component({
   selector: 'app-studies-content-ui',
   standalone: true,
   imports: [
     CommonModule,
-    TextStudyUiComponent,
     VideoStudyUiComponent,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    StudyContentTextComponent
   ],
   templateUrl: './studies-content-ui.component.html',
   styleUrl: './studies-content-ui.component.less',
@@ -26,7 +26,13 @@ export class StudiesContentUiComponent {
 
   @Output() openDial = new EventEmitter<boolean>()
 
+  @Output() updateStudy = new EventEmitter<boolean>()
+
   studyType:number = 0
+
+  studyUpdate(flag:boolean){
+    this.updateStudy.emit(flag)
+  }
 
   chooseType(){
     this.openDial.emit(true)
