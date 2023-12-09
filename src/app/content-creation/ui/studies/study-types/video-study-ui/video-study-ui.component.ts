43916a586405aa7f9abfc5d31fcb0bdbbb24d8c8
maxgotech <golovinmd@mail.ru>
2023-12-10 00:35:22 +0300

    ui компонент видео контента

--- src/app/content-creation/ui/studies/study-types/video-study-ui/video-study-ui.component.ts
+++ src/app/content-creation/ui/studies/study-types/video-study-ui/video-study-ui.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-video-study-ui',
@@ -11,4 +11,36 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './video-study-ui.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VideoStudyUiComponent { }
+export class VideoStudyUiComponent implements OnChanges { 
+  constructor(private cdr:ChangeDetectorRef){}
+
+  @Input() contentFromDB: any
+
+
+  @Output() saveVideo = new EventEmitter<FileList>()
+
+  path: string | null| ArrayBuffer = '';
+
+  onVideoSelected(event: any) {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event) => {
+      this.path = (<FileReader>event.target).result;
+      this.cdr.detectChanges()
+      }
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['contentFromDB'].currentValue != undefined){
+      this.path =this.contentFromDB.video_link
+    }
+  }
+
+  async SaveContent(files: FileList){
+    this.saveVideo.emit(files)
+  }
+
+}
