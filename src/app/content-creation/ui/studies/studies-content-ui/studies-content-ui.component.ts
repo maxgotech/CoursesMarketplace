8f44fa7ace81cda67883e13cf93577e301be41ec
maxgotech <golovinmd@mail.ru>
2023-12-10 00:35:54 +0300

    обновлено подключение видео компонента

--- src/app/content-creation/ui/studies/studies-content-ui/studies-content-ui.component.ts
+++ src/app/content-creation/ui/studies/studies-content-ui/studies-content-ui.component.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { VideoStudyUiComponent } from '../study-types/video-study-ui/video-study-ui.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { StudyContentTextComponent } from 'src/app/content-creation/features/studies/study-content-types/study-content-text/study-content-text.component';
+import { StudyContentVideoComponent } from 'src/app/content-creation/features/studies/study-content-types/study-content-video/study-content-video.component';
 
 @Component({
   selector: 'app-studies-content-ui',
   standalone: true,
   imports: [
     CommonModule,
-    VideoStudyUiComponent,
     MatDialogModule,
     MatButtonModule,
-    StudyContentTextComponent
+    StudyContentTextComponent,
+    StudyContentVideoComponent
   ],
   templateUrl: './studies-content-ui.component.html',
   styleUrl: './studies-content-ui.component.less',
