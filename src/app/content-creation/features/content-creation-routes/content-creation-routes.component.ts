bbb2b162d0f17577d2241bfa3b4c6956b06f8ff3
maxgotech <golovinmd@mail.ru>
2023-12-14 18:33:22 +0300

    руты на страницу контента модуля

--- src/app/content-creation/features/content-creation-routes/content-creation-routes.component.ts
+++ src/app/content-creation/features/content-creation-routes/content-creation-routes.component.ts
@@ -3,6 +3,7 @@ import { StudiesService } from "../../data-access/studies/studies.service";
 import { CourseContentComponent } from "../courses/course-content/course-content.component";
 import { CoursesCreationComponent } from "../courses/courses-creation/courses-creation.component";
 import { CoursesDescComponent } from "../courses/courses-desc/courses-desc.component";
+import { ModuleContentComponent } from "../courses/module-content/module-content.component";
 import { StudiesContentComponent } from "../studies/studies-content/studies-content.component";
 import { StudiesCreationComponent } from "../studies/studies-creation/studies-creation.component";
 
@@ -20,8 +21,12 @@ export default [
         component: CoursesDescComponent
       },
       {
-        path:':id/content',
-        component:CourseContentComponent
+        path: ':id/content',
+        component: CourseContentComponent
+      },
+      {
+        path: 'module/:id',
+        component: ModuleContentComponent
       }
     ]
   },
