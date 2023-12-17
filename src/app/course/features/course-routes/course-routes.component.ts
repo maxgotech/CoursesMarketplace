7021fe019d7f3c49a4768b98f543eba7740ba75a
maxgotech <golovinmd@mail.ru>
2023-12-17 15:24:27 +0300

    рут на страницу просмотра курса

--- src/app/course/features/course-routes/course-routes.component.ts
+++ src/app/course/features/course-routes/course-routes.component.ts
@@ -1,5 +1,6 @@
 import { CourseService } from "../../data-access/course.service";
 import { CoursePreviewComponent } from "../course-preview/course-preview.component";
+import { CourseSyllabusComponent } from "../course-syllabus/course-syllabus.component";
 
 export default [
   {
@@ -9,6 +10,10 @@ export default [
       {
         path: ':translit',
         component: CoursePreviewComponent
+      },
+      {
+        path:':cid/syllabus/study/:sid',
+        component: CourseSyllabusComponent
       }
     ]
   }
